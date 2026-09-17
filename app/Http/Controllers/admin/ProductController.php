c269d8e81cb9a871957053f076e6eee501e8ec15
Mohammed M. Salma <139702416+Mohammed-Salma@users.noreply.github.com>
2026-09-17 17:23:38 +0300

    Fetch & Create Products API (index - store) method and test it in postman

---
+++ app/Http/Controllers/admin/ProductController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+    //This method will return all the products
+    public function index()
+    {
+        $products = Product::orderBy('created_at', 'DESC')->get();
+        return response()->json([
+            'status' => 200,
+            'data' => $products,
+        ], 200);
+    }
+
+    //This method will store a new product
+    public function store(Request $request)
+    {
+        //Validate the request
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'price' => 'required|numeric',
+            'category' => 'required|integer',
+            'sku' => 'required|unique:products,sku',
+            'is_featured' => 'required',
+            'status' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'data' => $validator->errors(),
+            ], 400);
+        }
+
+        //Store the product
+        $product = new Product();
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->compare_price = $request->compare_price;
+        $product->category_id = $request->category;
+        $product->brand_id = $request->brand;
+        $product->sku = $request->sku;
+        $product->qty = $request->qty;
+        $product->description = $request->description;
+        $product->short_description = $request->short_description;
+        $product->status = $request->status;
+        $product->is_featured = $request->is_featured;
+        $product->barcode = $request->barcode;
+        $product->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => "Product has been created successfully",
+        ], 200);
+    }
+
+    //This method will return a single product
+    public function show($id) {}
+
+    //This method will update a product
+    public function update(Request $request, $id) {}
+
+    //This method will delete a product
+    public function destroy($id) {}
+}
